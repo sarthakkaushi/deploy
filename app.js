a066672cbae59b304b72670e61fa81e64d6fb537
unknown <sarthak95063@gmail.com>
2019-12-20 17:42:54 +0530

    body-parser

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express'),
       port = process.env.PORT || 5000,
       app = express(),
       path= require('path'),
-      indexRoute = require('./routes/index')
+      indexRoute = require('./routes/index'),
+      bodyParser = require('body-parser');
+
+      // support parsing of application/json type post data
+      app.use(bodyParser.json());
+      
+      //support parsing of application/x-www-form-urlencoded post data
+      app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/',indexRoute)
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
